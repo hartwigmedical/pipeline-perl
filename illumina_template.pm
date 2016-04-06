ac9bdb2f4fab2f7edd861cbb717d8760824f45d8
Arjen Wolfs <awolfs@schubergphilis.com>
2016-04-06 11:09:41 +0200

    Work in progress committed

---
+++ illumina_template.pm
@@ -0,0 +1,33 @@
+#!/usr/bin/perl -w
+
+###################################
+### illumina_template.pm
+### - Use templates to generate files
+###
+### Author: awolfs@schubergphilis.com
+###################################
+
+package illumina_template;
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT= qw(from_template);
+
+use strict;
+use Text::Template;
+use Env qw($TEMPLATES);
+
+my $template_dir = $TEMPLATES;
+
+sub from_template {
+	my $tname = shift || return undef;
+	my $outname = shift || return undef;
+	my %data = @_;
+	my $t = new Text::Template(TYPE => 'FILE', SOURCE=> "$template_dir/$tname");
+
+	my $tout;
+	open($tout, ">", "$outname") or die "Unable to open $tout for writing";
+	$t->fill_in(OUTPUT=>\*$tout, HASH => \%data) or die "Unable to process template $template_dir/$tname:$Text::Template::ERROR";  
+	close($tout);
+};
+
+1;
