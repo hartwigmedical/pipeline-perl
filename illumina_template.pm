489adbf7372b2a065518c49787d555bc33a9727e
Arjen Wolfs <awolfs@schubergphilis.com>
2016-04-07 10:08:22 +0200

    Removed Text::Template refs

--- illumina_template.pm
+++ illumina_template.pm
@@ -13,7 +13,6 @@ require Exporter;
 @EXPORT= qw(from_template);
 
 use strict;
-use Text::Template;
 use Template;
 use Env qw($TEMPLATES);
 
@@ -28,18 +27,6 @@ sub from_template {
 	my $tout;
 	open($tout, ">", "$outname") or die "Unable to open $tout for writing";
 	$t->process($tname, \%data, \*$tout) or die $t->error();
-	close($tout);
-};
-
-sub from_template3 {
-	my $tname = shift || return undef;
-	my $outname = shift || return undef;
-	my %data = @_;
-	my $t = new Text::Template(TYPE => 'FILE', SOURCE=> "$template_dir/$tname");
-
-	my $tout;
-	open($tout, ">", "$outname") or die "Unable to open $tout for writing";
-	$t->fill_in(OUTPUT=>\*$tout, HASH => \%data) or die "Unable to process template $template_dir/$tname:$Text::Template::ERROR";  
 	close($tout);
 };
 
