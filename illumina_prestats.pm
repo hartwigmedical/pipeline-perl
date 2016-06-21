1d0c5854ee3ab2c0ddb39ef49aa694c46de69045
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2016-06-21 13:18:39 +0200

    Refactored prestats

--- illumina_prestats.pm
+++ illumina_prestats.pm
@@ -1,12 +1,5 @@
 #!/usr/bin/perl -w
 
-###################################
-### illumina_prestats.pm
-### - Run fastqc for each fastq
-###
-### Author: S.W.Boymans & H.H.D.Kerstens
-###################################
-
 package illumina_prestats;
 
 use strict;
@@ -16,9 +9,6 @@ use illumina_sge;
 use illumina_template;
 
 sub runPreStats {
-    ###
-    # Run fastqc
-    ###
     my $configuration = shift;
     my %opt = %{$configuration};
     my $jobIds = {};
@@ -28,25 +18,23 @@ sub runPreStats {
 
     my @qsubOut = ();
     my $runName = (split("/", $opt{OUTPUT_DIR}))[-1];
-    foreach my $input (keys %{$opt{FASTQ}}){
-	my $coreName = undef;
-	$coreName = (split("/", $input))[-1];
-	$coreName =~ s/\.fastq.gz//;
-	my ($sampleName) =  split("_", $coreName);
-	print "\t$input\n"; #print fastq filename
+    foreach my $input (keys %{$opt{FASTQ}}) {
+		my $coreName = undef;
+		$coreName = (split("/", $input))[-1];
+		$coreName =~ s/\.fastq.gz//;
+		my ($sampleName) =  split("_", $coreName);
+		print "\t$input\n";
 
-	if(! -e "$opt{OUTPUT_DIR}/$sampleName/logs/PreStats_$sampleName.done"){
-
-	    my $preStatsJobId = "PreStat_$coreName\_".get_job_id();
-	    my $preStatsFile = "$opt{OUTPUT_DIR}/$sampleName/jobs/$preStatsJobId.sh";
-	    push(@{$jobIds->{$sampleName}}, $preStatsJobId);
-	    from_template("PreStat.sh.tt", $preStatsFile, sampleName => $sampleName, coreName => $coreName, input => $input, opt => \%opt, runName => $runName);
-	    my $qsub = &qsubTemplate(\%opt,"PRESTATS");
-	    push(@qsubOut, "$qsub -o $opt{OUTPUT_DIR}/$sampleName/logs/PreStat_$coreName.out -e $opt{OUTPUT_DIR}/$sampleName/logs/PreStats_$coreName.err -N $preStatsJobId $opt{OUTPUT_DIR}/$sampleName/jobs/$preStatsJobId.sh");
-	} else {
-	    print "\t WARNING: FASTQC report for $input already exists, skipping.\n";
-	}
-
+		if (! -e "$opt{OUTPUT_DIR}/$sampleName/logs/PreStats_$sampleName.done") {
+			my $preStatsJobId = "PreStat_$coreName\_".get_job_id();
+			my $preStatsFile = "$opt{OUTPUT_DIR}/$sampleName/jobs/$preStatsJobId.sh";
+			push(@{$jobIds->{$sampleName}}, $preStatsJobId);
+			from_template("PreStat.sh.tt", $preStatsFile, sampleName => $sampleName, coreName => $coreName, input => $input, opt => \%opt, runName => $runName);
+			my $qsub = &qsubTemplate(\%opt,"PRESTATS");
+			push(@qsubOut, "$qsub -o $opt{OUTPUT_DIR}/$sampleName/logs/PreStat_$coreName.out -e $opt{OUTPUT_DIR}/$sampleName/logs/PreStats_$coreName.err -N $preStatsJobId $opt{OUTPUT_DIR}/$sampleName/jobs/$preStatsJobId.sh");
+		} else {
+			print "\t WARNING: FASTQC report for $input already exists, skipping.\n";
+		}
     }
 
     from_template("PreStatsMainJob.sh.tt", $mainJobID, qsubOut => \@qsubOut, opt => \%opt);
