11979cac7f45a14ef5cdaec29ac92aba4463b557
Sam Brightman <s.brightman@hartwigmedicalfoundation.nl>
2016-11-10 13:28:21 +0100

    Fix BAF script bugs

--- scripts/makeBAFplot.R
+++ scripts/makeBAFplot.R
@@ -73,45 +73,47 @@ for (i in c(1:length(chromosomes))) {
       peaks <- determine_peaks(region)
       #print(nrow(peaks))
       df <- rbind(df, peaks[1:2,])
-    }
-
-    # check if HETRO / HOMOZYGOUS calls present
-    if (peaks$baf[1] <= 0.1 || peaks$baf[1] >= 0.9) {
-      # RE-RUN for intermediate BAFs
-      region <- subset(region, baf > 0.1 & baf < 0.9)
-      if (nrow(region) >= 100) {
-        peaks <- determine_peaks(region)
-        #print(nrow(peaks))
-        df <- rbind(df, peaks)
+      # check if HETRO / HOMOZYGOUS calls present
+      if (peaks$baf[1] <= 0.1 || peaks$baf[1] >= 0.9) {
+        # RE-RUN for intermediate BAFs
+        region <- subset(region, baf > 0.1 & baf < 0.9)
+        if (nrow(region) >= 100) {
+          peaks <- determine_peaks(region)
+          #print(nrow(peaks))
+          df <- rbind(df, peaks)
+        }
       }
     }
   }
-
 }
 
-df$baf <- round(df$baf,3)
-df <- df[!is.na(df$CHROM),]
-df$CHROM <- factor(df$CHROM, levels=chromosomes)
-#rm(rowdat)
+if (nrow(df) > 0) {
+  df$baf <- round(df$baf,3)
+  df <- df[!is.na(df$CHROM),]
+  df$CHROM <- factor(df$CHROM, levels=chromosomes)
+  #rm(rowdat)
+}
 
-pdf(file=paste0(outdir,sample,"_BAF.pdf"), width=15, height=2, pointsize=6, useDingbats=FALSE)
+pdf(file=file.path(outdir, paste0(sample, "_BAF.pdf")), width=15, height=2, pointsize=6, useDingbats=FALSE)
 
-p <- ggplot(df, aes(POS, baf, group=CHROM)) + geom_point(shape=20, size=0.2, alpha=0.5) + facet_wrap(~CHROM, nrow=1, scales="free_x")
+if (nrow(df) > 0) {
+  p <- ggplot(df, aes(POS, baf, group=CHROM)) + geom_point(shape=20, size=0.2, alpha=0.5) + facet_wrap(~CHROM, nrow=1, scales="free_x")
 
-p <- p + theme(
-axis.line.x=element_blank(),
-axis.text.x=element_blank(),
-axis.ticks.x=element_blank(),
+  p <- p + theme(
+    axis.line.x=element_blank(),
+    axis.text.x=element_blank(),
+    axis.ticks.x=element_blank(),
 
-panel.grid.minor=element_blank(),
-panel.grid.major.x=element_blank(),
-panel.grid.major.y=element_line(colour="grey80", linetype=2, size=0.2),
+    panel.grid.minor=element_blank(),
+    panel.grid.major.x=element_blank(),
+    panel.grid.major.y=element_line(colour="grey80", linetype=2, size=0.2),
 
-legend.position="none",
-panel.background=element_blank(),
-panel.border=element_blank(),
-plot.background=element_blank())
+    legend.position="none",
+    panel.background=element_blank(),
+    panel.border=element_blank(),
+    plot.background=element_blank())
 
-print(p)
-dev.off()
-rm(p)
+  print(p)
+  dev.off()
+  rm(p)
+}
