fbab45a8d7e8f7ac61862b069f78655c9ac27921
Mircea Voda <mircearaul.voda@gmail.com>
2017-07-28 16:16:30 +0200

    add few more log messages

--- lib/HMF/Pipeline/SomaticVariants.pm
+++ lib/HMF/Pipeline/SomaticVariants.pm
@@ -27,12 +27,14 @@ sub run {
     my $dirs = createDirs(catfile($opt->{OUTPUT_DIR}, "somaticVariants", $joint_name));
 
     if (index($tumor_bam_path, "recalibrated.bam") == -1) {
+        say "Missing recalibrated file for sample: $tumor_sample";
         HMF::Pipeline::BaseRecalibration::runRecalibrationOnSample($tumor_sample, $opt);
         my ($tumor_sample_bam, $tumor_sample_jobs) = sampleBamAndJobs($tumor_sample, $opt);
         $tumor_bam_path = $tumor_sample_bam;
         $running_jobs = [ uniq @{$running_jobs}, @{$tumor_sample_jobs} ];
     }
 
+    say "Running somatic callers on:";
     say "\n$joint_name \t $ref_bam_path \t $tumor_bam_path";
 
     my $done_file = checkReportedDoneFile($joint_name, undef, $dirs, $opt) or return;
