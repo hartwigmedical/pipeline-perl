b8caa7ec666e79542efdd1fbada5ab016d718e0f
Jonathan Baber <jonbaber@gmail.com>
2017-08-02 14:40:19 +1000

    Added mock cobalt job

--- lib/HMF/Pipeline/Cobalt.pm
+++ lib/HMF/Pipeline/Cobalt.pm
@@ -6,29 +6,37 @@ use discipline;
 use File::Spec::Functions;
 
 use HMF::Pipeline::Config qw(createDirs sampleBamAndJobs);
-use HMF::Pipeline::Sge qw(qsubJava);
 use HMF::Pipeline::Job qw(fromTemplate);
 use HMF::Pipeline::Metadata qw(linkExtraArtefact);
+use HMF::Pipeline::Sge qw(qsubJava);
 
 use parent qw(Exporter);
 our @EXPORT_OK = qw(run);
 
-
 sub run {
     my ($opt) = @_;
 
-    say "\n### SCHEDULING COBALT ###";
+    say "\n### SCHEDULING COBALT ANALYSIS ###";
 
-    my $dirs = createDirs($opt->{OUTPUT_DIR}, cobalt => "cobalt");
-
-    my @baf_jobs;
     foreach my $sample (keys %{$opt->{SAMPLES}}) {
         my ($sample_bam, $running_jobs) = sampleBamAndJobs($sample, $opt);
+        my $dirs = createDirs($opt->{OUTPUT_DIR}, cobalt => "cobalt");
 
-        say "COBALT Sample:" . $sample . " BAM:" . $sample_bam;
+        my $job_id = fromTemplate(
+            "Cobalt",
+            $sample,
+            1,
+            qsubJava($opt, "COBALT"),
+            $running_jobs,
+            $dirs,
+            $opt,
+            sample => $sample,
+            sample_bam => $sample_bam,
+        );
+        next unless $job_id;
+
+        push @{$opt->{RUNNING_JOBS}->{$sample}}, $job_id;
     }
-
-
     return;
 }
 
