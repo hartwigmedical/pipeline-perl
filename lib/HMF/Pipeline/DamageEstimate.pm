285511421a4ca03fa7d228ff573e18e4fb01f34a
Stef van Lieshout <stefvanlieshout@fastmail.fm>
2017-08-02 17:04:04 +0200

    cleaned up damageEstimator settings/output-location/logs/filenames/etc (minimal pipeline run succeeded without err)

--- lib/HMF/Pipeline/DamageEstimate.pm
+++ lib/HMF/Pipeline/DamageEstimate.pm
@@ -2,11 +2,9 @@ package HMF::Pipeline::DamageEstimate;
 
 use FindBin::libs;
 use discipline;
-
 use File::Spec::Functions;
-
 use HMF::Pipeline::Job qw(fromTemplate checkReportedDoneFile markDone);
-use HMF::Pipeline::Config qw(createDirs sampleControlBamsAndJobs);
+use HMF::Pipeline::Config qw(createDirs sampleBamAndJobs);
 use HMF::Pipeline::Sge qw(qsubJava);
 
 use parent qw(Exporter);
@@ -15,36 +13,37 @@ our @EXPORT_OK = qw(run);
 sub run {
     my ($opt) = @_;
 
-    my ($ref_sample, $tumor_sample, $ref_bam_path, $tumor_bam_path, $joint_name, $running_jobs) = sampleControlBamsAndJobs($opt);
-    my $dirs = createDirs($opt->{OUTPUT_DIR}, damageEstimate => "damageEstimate");
-
     say "\n### SCHEDULING DAMAGE ESTIMATE ###";
 
-    my $ref_job_id = fromTemplate(
-        "DamageEstimate",
-        undef,
-        1,
-        qsubJava($opt, "DAMAGE_ESTIMATE"),
-        $running_jobs,
-        $dirs,
-        $opt,
-        damage_estimate_bam_path => $ref_bam_path,
-        joint_name => $joint_name
-    );
-
-    my $tumor_job_id = fromTemplate(
-        "DamageEstimate",
-        undef,
-        1,
-        qsubJava($opt, "DAMAGE_ESTIMATE"),
-        $running_jobs,
-        $dirs,
-        $opt,
-        damage_estimate_bam_path => $tumor_bam_path,
-        joint_name => $joint_name
-    );
+    foreach my $sample (keys %{$opt->{SAMPLES}}) {
+        my $dirs = createDirs( catfile( $opt->{OUTPUT_DIR}, $sample ), damageEstimate => "damageEstimate" );
+        my ($bam_path, $running_jobs) = sampleBamAndJobs($sample, $opt);
+        my ($job_id) = runDamageEstimate($sample, $bam_path, $running_jobs, $dirs, $opt);
+        push @{$opt->{RUNNING_JOBS}->{damageEstimate}}, $job_id;
+    }
 
     return;
+}
+
+sub runDamageEstimate{
+    my ($sample, $bam_path, $running_jobs, $dirs, $opt) = @_;
+    my $out_dir_path = $dirs->{ 'damageEstimate' };
+    
+    say "Running damageEstimate for: $sample";
+    my $job_id = fromTemplate(
+        "DamageEstimate",
+        $sample,
+        1,
+        qsubJava($opt, "DAMAGE_ESTIMATE"),
+        $running_jobs,
+        $dirs,
+        $opt,
+        sample => $sample,
+        damage_estimate_bam_path => $bam_path,
+        damage_estimate_out_path => $out_dir_path,
+        joint_name => "DamageEstimate_".$sample,
+    );
+    return ($job_id);
 }
 
 1;
