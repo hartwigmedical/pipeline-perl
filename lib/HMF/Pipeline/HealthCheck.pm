20b3b0150ec8ea1e266bb9f45e6c2842b8b76be6
Mircea Voda <mircearaul.voda@gmail.com>
2017-08-18 13:43:10 +0200

    add healthcheck to running_jobs and add output to extras archives

--- lib/HMF/Pipeline/HealthCheck.pm
+++ lib/HMF/Pipeline/HealthCheck.pm
@@ -2,9 +2,14 @@ package HMF::Pipeline::HealthCheck;
 
 use FindBin::libs;
 use discipline;
+
+use File::Find::Rule;
+use File::Spec::Functions;
+
 use HMF::Pipeline::Job qw(fromTemplate);
 use HMF::Pipeline::Sge qw(qsubJava);
 use HMF::Pipeline::Config qw(allRunningJobs createDirs);
+use HMF::Pipeline::Metadata;
 
 use parent qw(Exporter);
 our @EXPORT_OK = qw(run
@@ -16,6 +21,7 @@ sub run {
     say "\n### SCHEDULING HEALTH CHECKER ###";
 
     my $dirs = createDirs($opt->{OUTPUT_DIR});
+    my $health_check_dir = $dirs->{log};
 
     my $job_id = fromTemplate(
         "HealthCheck",
@@ -26,10 +32,16 @@ sub run {
         $dirs,
         $opt,
         run_dir => $opt->{OUTPUT_DIR},
-        output_path => $dirs->{log},
+        output_path => $health_check_dir,
     );
 
+    push @{$opt->{RUNNING_JOBS}->{healthcheck}}, $job_id;
+
     # TODO: run qc scripts
+
+    my @health_check_files = File::Find::Rule->file()->name("*_health_checks.json")->in($health_check_dir);
+    my $health_check_file = $health_check_files[0];
+    HMF::Pipeline::Metadata::linkExtraArtefact(catfile($health_check_dir, $health_check_file), $opt);
 
     return;
 }
