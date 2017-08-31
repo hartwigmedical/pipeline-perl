dc56e37b5ded8668612761cac3562a89951d90f2
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2017-08-31 14:39:38 +0200

    Refactored strelka post process (wip)

--- lib/HMF/Pipeline/SomaticVariants.pm
+++ lib/HMF/Pipeline/SomaticVariants.pm
@@ -24,8 +24,6 @@ sub run {
 
     my ($ref_sample, $tumor_sample, $ref_bam_path, $tumor_bam_path, $joint_name, $running_jobs) = sampleControlBamsAndJobs($opt);
     my $dirs = createDirs(catfile($opt->{OUTPUT_DIR}, "somaticVariants", $joint_name));
-    my $final_vcf = catfile($dirs->{out}, "${joint_name}_post_processed.vcf");
-    $opt->{SOMVAR_VCF_FILE} = $final_vcf;
 
     my ($recalibrated_ref_bam, $recal_ref_jobs) = checkRecalibratedSample($ref_sample, $ref_bam_path, $opt);
     my ($recalibrated_tumor_bam, $recal_tumor_jobs) = checkRecalibratedSample($tumor_sample, $tumor_bam_path, $opt);
@@ -35,21 +33,25 @@ sub run {
     say "$joint_name \t $recalibrated_ref_bam \t $recalibrated_tumor_bam";
     my $done_file = checkReportedDoneFile("Somatic_$joint_name", undef, $dirs, $opt) or return;
 
-    my ($job_id, $vcf) = runStrelka($tumor_sample, $recalibrated_ref_bam, $recalibrated_tumor_bam, $joint_name, $running_jobs, $dirs, $opt);
-    push @{$opt->{RUNNING_JOBS}->{somvar}}, $job_id;
+    my ($strelka_job_id, $strelka_vcf) = runStrelka($tumor_sample, $recalibrated_ref_bam, $recalibrated_tumor_bam, $joint_name, $running_jobs,
+        $dirs, $opt);
+    push @{$opt->{RUNNING_JOBS}->{somvar}}, $strelka_job_id;
 
-    my $post_process_job_ids = postProcessStrelka($joint_name, $final_vcf, $job_id, $vcf, $dirs, $opt);
-    push @{$opt->{RUNNING_JOBS}->{somvar}}, @{$post_process_job_ids};
+    my $final_vcf = catfile($dirs->{out}, "${joint_name}_post_processed.vcf");
+    $opt->{SOMVAR_VCF_FILE} = $final_vcf;
 
-    $job_id = markDone($done_file, [ $job_id, @{$post_process_job_ids} ], $dirs, $opt);
-    push @{$opt->{RUNNING_JOBS}->{somvar}}, $job_id;
+    my $post_process_job_id = postProcessStrelka($joint_name, $final_vcf, $strelka_job_id, $strelka_vcf, $tumor_sample, $dirs, $opt);
+    push @{$opt->{RUNNING_JOBS}->{somvar}}, $post_process_job_id;
+
+    my $done_job_id = markDone($done_file, [ $strelka_job_id, $post_process_job_id ], $dirs, $opt);
+    push @{$opt->{RUNNING_JOBS}->{somvar}}, $done_job_id;
     return;
 }
 
 sub checkRecalibratedSample {
     my ($sample, $sample_bam_path, $opt) = @_;
 
-    if (index($sample_bam_path, "recalibrated.bam") == -1) {
+    if (index($sample_bam_path, "recalibrated.bam") == - 1) {
         say "\nMissing recalibrated file for sample: $sample";
         my ($recalibrated_bam, $recalibration_jobs) = HMF::Pipeline::BaseRecalibration::runRecalibrationOnSample($sample, $opt);
         return ($recalibrated_bam, $recalibration_jobs);
@@ -63,7 +65,7 @@ sub runStrelka {
     say "\n### SCHEDULING STRELKA ###";
 
     $dirs->{strelka}->{out} = addSubDir($dirs, "strelka");
-    my $final_vcf = catfile($dirs->{strelka}->{out}, "passed.somatic.merged.vcf");
+    my $output_vcf = catfile($dirs->{strelka}->{out}, "passed.somatic.merged.vcf");
 
     my $job_id = fromTemplate(
         "Strelka",
@@ -73,64 +75,38 @@ sub runStrelka {
         $running_jobs,
         $dirs,
         $opt,
-        tumor_sample => $tumor_sample,
-        joint_name => $joint_name,
-        ref_bam_path => $ref_bam_path,
+        tumor_sample   => $tumor_sample,
+        joint_name     => $joint_name,
+        ref_bam_path   => $ref_bam_path,
         tumor_bam_path => $tumor_bam_path,
-        final_vcf => $final_vcf,
+        final_vcf      => $output_vcf,
     );
 
-    return ($job_id, $final_vcf);
+    return ($job_id, $output_vcf);
 }
 
 sub postProcessStrelka {
-    my ($joint_name, $final_vcf, $strelka_job_id, $strelka_vcf, $dirs, $opt) = @_;
+    my ($joint_name, $final_vcf, $strelka_job_id, $strelka_vcf, $tumor_sample, $dirs, $opt) = @_;
 
     say "\n### SCHEDULING STRELKA POST PROCESS ###";
 
-    my @job_ids;
-    my $qsub = qsubJava($opt, "SOMVARANNOTATE");
-    my $output_vcf = $strelka_vcf;
-    my $job_id = $strelka_job_id;
-
-    my $pre_annotate_vcf = $output_vcf;
-    if ($opt->{SOMVAR_ANNOTATE} eq "yes") {
-        (my $basename = $output_vcf) =~ s/\.vcf$//;
-        $output_vcf = "${basename}_annotated.vcf";
-
-        $job_id = fromTemplate(
-            "SomaticAnnotation",
-            undef,
-            0,
-            $qsub,
-            [$job_id],
-            $dirs,
-            $opt,
-            basename => $basename,
-            final_vcf => $output_vcf,
-            joint_name => $joint_name,
-        );
-        push @job_ids, $job_id;
-    }
-
-    $job_id = fromTemplate(
-        "SomaticPONAnnotation",
+    my $job_id = fromTemplate(
+        "StrelkaPostProcess",
         undef,
         0,
-        $qsub,
-        [$job_id],
+        qsubJava($opt, "STRELKAPOSTPROCESS"),
+        [ $strelka_job_id ],
         $dirs,
         $opt,
-        pre_annotate_vcf => $pre_annotate_vcf,
-        input_vcf => $output_vcf,
-        output_vcf => $final_vcf,
-        joint_name => $joint_name,
+        tumor_sample => $tumor_sample,
+        strelka_vcf  => $strelka_vcf,
+        final_vcf    => $final_vcf,
+        joint_name   => $joint_name,
     );
-    push @job_ids, $job_id;
 
     HMF::Pipeline::Metadata::linkVcfArtefacts($final_vcf, "somatic", $opt) if $job_id;
 
-    return \@job_ids;
+    return $job_id;
 }
 
 1;
